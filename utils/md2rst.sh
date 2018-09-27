db29a98a7427c67127bb4170628f7f6fb11bec0b
pytlab <shaozhengjiang@gmail.com>
2018-09-27 20:03:26 +0800

    Modify script

--- utils/md2rst.sh
+++ utils/md2rst.sh
@@ -5,7 +5,7 @@ for f in $(ls *.md)
 do
     filename=$(cut -d'.' -f1 <<< ${f})
     pandoc --from=markdown --to=rst --output=${filename}.rst ${filename}.md
-    echo "Convert ${filename}.rst -> ${filename}.md..."
+    echo "Convert ${filename}.md -> ${filename}.rst..."
 done
 echo "Done"
 
